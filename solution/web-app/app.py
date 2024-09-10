e16fe5870c53a765d25f7fe22ff1b3bfec741af3
szauer.szilard <szauerszilard@stud.uni-obuda.hu>
2024-09-10 22:26:07 +0200

    lungs prediction fixed

--- solution/web-app/app.py
+++ solution/web-app/app.py
@@ -1,13 +1,16 @@
 import pickle
 import numpy as np
 from flask import Flask, request, render_template
+from PIL import Image
+from keras.models import load_model
+from keras.layers import TFSMLayer
+import tensorflow as tf
 
 app = Flask(__name__)
 
-# Load the trained RandomForestClassifier model
-model = pickle.load(open("../heart-failure_model.pkl", "rb"))
+heart_model = pickle.load(open("../heart-failure_model.pkl", "rb"))
+lungs_model = load_model('../lungs_class_model.keras')
 
-# Example patients data
 patients_data = [
     {"name": "John Doe", "sex": "M", "place_of_birth": "Seattle", "date_of_birth": "1999-01-01"},
     {"name": "Jane Smith", "sex": "F", "place_of_birth": "Washington", "date_of_birth": "1998-01-28"},
@@ -17,8 +20,42 @@ patients_data = [
 def home():
     return render_template("index.html")
 
-@app.route("/predict", methods=["POST"])
-def predict():
+@app.route("/patients")
+def patients():
+    return render_template("patients.html", patients=patients_data)
+
+@app.route("/search_patient", methods=["GET"])
+def search_patient():
+    query = request.args.get('query', '').lower()
+    filtered_patients = [p for p in patients_data if query in p['name'].lower()]
+    return render_template("patients.html", patients=filtered_patients)
+
+@app.route("/register_patient", methods=["POST"])
+def register_patient():
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    sex = request.form.get("sex")
+    place_of_birth = request.form.get("place_of_birth")
+    date_of_birth = request.form.get("date_of_birth")
+    
+    full_name = f"{first_name} {last_name}"
+    
+    new_patient = {
+        "name": full_name,
+        "sex": "M" if sex == "0" else "F",
+        "place_of_birth": place_of_birth,
+        "date_of_birth": date_of_birth,
+    }
+    patients_data.append(new_patient)
+    
+    return render_template("patients.html", patients=patients_data)
+
+@app.route("/heart")
+def heart():
+    return render_template("heart.html")
+
+@app.route("/predict_heart", methods=["POST"])
+def predict_heart():
     chest_pain_map = {'TA': 0, 'ATA': 1, 'NAP': 2, 'ASY': 3}
     resting_ecg_map = {'Normal': 0, 'ST': 1, 'LVH': 2}
     st_slope_map = {'Up': 0, 'Flat': 1, 'Down': 2}
@@ -35,7 +72,7 @@ def predict():
             int_features.append(float(value))
     
     final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    prediction = heart_model.predict(final_features)
     output = int(prediction[0])
     
     return render_template(
@@ -43,42 +80,34 @@ def predict():
         prediction_text=f"{'Positive' if output == 1 else 'Negative'}"
     )
 
-@app.route("/patients")
-def patients():
-    return render_template("patients.html", patients=patients_data)
 
-@app.route("/search_patient", methods=["GET"])
-def search_patient():
-    query = request.args.get('query', '').lower()
-    filtered_patients = [p for p in patients_data if query in p['name'].lower()]
-    return render_template("patients.html", patients=filtered_patients)
+@app.route("/covid")
+def covid():
+    return render_template("covid.html")
 
-@app.route("/register_patient", methods=["POST"])
-def register_patient():
-    # Get the form data
-    first_name = request.form.get("first_name")
-    last_name = request.form.get("last_name")
-    sex = request.form.get("sex")
-    place_of_birth = request.form.get("place_of_birth")
-    date_of_birth = request.form.get("date_of_birth")
-    
-    # Combine first and last name
-    full_name = f"{first_name} {last_name}"
-    
-    # Add the new patient to the patients_data list
-    new_patient = {
-        "name": full_name,
-        "sex": "M" if sex == "0" else "F",
-        "place_of_birth": place_of_birth,
-        "date_of_birth": date_of_birth,
-    }
-    patients_data.append(new_patient)
-    
-    return render_template("patients.html", patients=patients_data)
 
-@app.route("/heart")
-def heart():
-    return render_template("heart.html")
+@app.route("/predict_lungs", methods=["POST"])
+def predict_lungs():
+    file = request.files['image_file']
+    if file.filename == '':
+        return render_template("covid.html", prediction_text="No selected file.")
+    
+    img = Image.open(file)
+    img = img.resize((64, 64))
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    
+    prediction = lungs_model.predict(img_array)
+    result = np.argmax(prediction, axis=1)[0]
+    if result==0:
+        result="Covid"
+    elif result==1:
+        result="Normal"
+    else:
+        result="Viral Pneumonia"
+
+    return render_template("covid.html", prediction_text=f"{result}")
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
